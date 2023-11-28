756986b4a3dde72d4080777f9d23815fd9adfcd8
dmitrygvl <dmitrygv1994@ya.ru>
2023-11-28 15:52:39 +0300

    Add TS & babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,50 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
     //   "jest/globals": true,
-    },
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "jest"],
-    extends: [
-      "airbnb-base",
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+    //    "jest"
+  ],
+  extends: [
+    "airbnb-base",
     //   "plugin:jest/recommended",
-      "plugin:@typescript-eslint/recommended",
-      "prettier",
-    ],
-    overrides: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  ignorePatterns: ["main.js"],
+  rules: {
+    "max-len": [
+      "error",
       {
-        env: {
-          node: true,
-        },
-        files: [".eslintrc.{js,cjs}"],
-        parserOptions: {
-          sourceType: "script",
-        },
+        code: 140,
+        ignoreComments: true,
       },
     ],
-    ignorePatterns: ["main.js"],
-    rules: {
-      "max-len": [
-        "error",
-        {
-          code: 140,
-          ignoreComments: true,
-        },
-      ],
-      "@typescript-eslint/no-unused-vars": 0,
-      "@typescript-eslint/no-var-requires": 0,
-      "import/prefer-default-export": "off",
-      "no-promise-executor-return": "off",
-      "no-unused-vars": "off",
-      "no-plusplus": "off",
-      "no-shadow": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-shadow": "warn",
-      "import/no-unresolved": "off", // https://github.com/typescript-eslint/typescript-eslint/issues/1624
-      "import/extensions": ["warn", "never"], // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
-    },
-  };
+    "@typescript-eslint/no-unused-vars": 0,
+    "@typescript-eslint/no-var-requires": 0,
+    "import/prefer-default-export": "off",
+    "no-promise-executor-return": "off",
+    "no-unused-vars": "off",
+    "no-plusplus": "off",
+    "no-shadow": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-shadow": "warn",
+    "import/no-unresolved": "off", // https://github.com/typescript-eslint/typescript-eslint/issues/1624
+    "import/extensions": ["warn", "never"], // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
+  },
+};
---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: "current" },
+        useBuiltIns: "entry", // alternative mode: "entry"/ "usage"
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+};
