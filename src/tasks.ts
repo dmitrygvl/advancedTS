e043c84cc37663208a57375843b68ddafe3377c1
dmitrygvl <dmitrygv1994@ya.ru>
2023-11-29 14:51:14 +0300

    Add task2

--- src/tasks.ts
+++ src/tasks.ts
@@ -41,39 +41,41 @@ export const getUserOrderStates = (
 // // eslint-disable-next-line @typescript-eslint/no-explicit-any
 // type FIXME = any;
 
-// type Order =
-//   | {
-//       state: "initial";
-//       sum: number;
-//     }
-//   | {
-//       state: "inWork";
-//       sum: number;
-//       workerId: number;
-//     }
-//   | {
-//       state: "buyingSupplies";
-//       sum: number;
-//       workerId: number;
-//       suppliesSum: number;
-//     }
-//   | {
-//       state: "producing";
-//       sum: number;
-//       workerId: number;
-//       suppliesSum: number;
-//       produceEstimate: Date;
-//     }
-//   | {
-//       state: "fullfilled";
-//       sum: number;
-//       workerId: number;
-//       suppliesSum: number;
-//       produceEstimate: Date;
-//       fullfillmentDate: Date;
-//     };
+type Order =
+  | {
+      state: "initial";
+      sum: number;
+    }
+  | {
+      state: "inWork";
+      sum: number;
+      workerId: number;
+    }
+  | {
+      state: "buyingSupplies";
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+    }
+  | {
+      state: "producing";
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+      produceEstimate: Date;
+    }
+  | {
+      state: "fullfilled";
+      sum: number;
+      workerId: number;
+      suppliesSum: number;
+      produceEstimate: Date;
+      fullfillmentDate: Date;
+    };
 
-// export const getOrderState = (order: Order): FIXME => order.state;
+  type OrderStates = Order['state'] 
+
+export const getOrderState = (order: Order): OrderStates => order.state;
 
 // // Есть общая функция omit которая удаляет поле из объекта и возвращает его без этого поля
 // // Нужно заменить FIXME на соответствующий тип
